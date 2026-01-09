1b11b3c899610f3bfce6c06aa8d8ab55f47515b1
Badil <badilarrohman48@gmail.com>
2026-01-10 03:57:24 +0700

    fix bug filter by category

--- Blog/script.js
+++ Blog/script.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const filterBtns = document.querySelectorAll(".filter-btn");
   const cards = document.querySelectorAll(".filter-item");
-  const noPostMsg = document.getElementById("no-post-msg");
 
   filterBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       filterBtns.forEach((b) => b.classList.remove("active"));
       btn.classList.add("active");
 
-      const category = btn.getAttribute("onclick").split("'")[1];
+      if (btn.tagName === "A") return;
 
+      const category = btn.getAttribute("data-category");
       filterSelection(category);
     });
   });
 
   function filterSelection(category) {
     let visibleCount = 0;
+    const noPostMsg = document.getElementById("no-post-msg");
 
     cards.forEach((card) => {
       card.classList.remove("fade-in");
 
+      if (card.id === "no-post-msg") return;
+
       if (category === "all" || card.classList.contains(category)) {
         card.classList.remove("hide");
-
         void card.offsetWidth;
         card.classList.add("fade-in");
-
         visibleCount++;
       } else {
         card.classList.add("hide");
@@ -38,7 +39,6 @@ document.addEventListener("DOMContentLoaded", () => {
         noPostMsg.classList.add("fade-in");
       } else {
         noPostMsg.classList.add("hide");
-        noPostMsg.classList.remove("fade-in");
       }
     }
   }
@@ -50,5 +50,18 @@ document.addEventListener("DOMContentLoaded", () => {
         preloader.classList.add("hide");
       }, 500);
     });
+  }
+});
+
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("keydown", (e) => {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey &&
+      e.shiftKey &&
+      (e.key === "I" || e.key === "J" || e.key === "C")) ||
+    (e.ctrlKey && e.key === "u")
+  ) {
+    e.preventDefault();
   }
 });
