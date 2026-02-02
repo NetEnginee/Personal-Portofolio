2d7055bbb10046a2d17608eb2d2bcf60f05bc792
Badil <badilarrohman48@gmail.com>
2026-02-03 04:06:35 +0700

    fix not working caching system

--- components/javascript/cache.js
+++ components/javascript/cache.js
@@ -2,7 +2,7 @@ export default function cache() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("sw.js")
+        .register("/sw.js")
         .then((reg) =>
           console.log("Service Worker: Registered (Scope: " + reg.scope + ")"),
         )
---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "badil-portofolio-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style/style.css",
+  "/script.js",
+  "assets/image/admin/Personal Picture.webp",
+  "assets/image/project/pomodoro.webp",
+  "assets/image/project/taskly.webp",
+  "assets/image/project/personal portofolio.webp",
+  "https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(ASSETS_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      const networkFetch = fetch(e.request).then((networkResponse) => {
+        const responseToCache = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, responseToCache);
+        });
+
+        return networkResponse;
+      });
+
+      return cachedResponse || networkFetch;
+    }),
+  );
+});
